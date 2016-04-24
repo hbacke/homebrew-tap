80d780ccf09227e02988dc309030eb6e439ada1d
Henrik Backe <henrik@backe.eu>
2016-04-24 18:27:37 +0200

    Stuff

---
+++ Casks/samsung-printer-drivers.rb
@@ -0,0 +1,13 @@
+cask 'samsung-printer-drivers' do
+  version '2.6'
+  sha256 'ecf283ff40df816e84d3a6148676114d2b4fe3f4074feb995c2c7e0be8be4964'
+
+  url 'http://support.apple.com/downloads/DL905/en_US/SamsungPrinterDrivers2.6.dmg'
+  name 'Samsung Printer Drivers'
+  homepage 'http://backe.eu'
+  license :unknown
+
+  pkg 'SamsungPrinterDrivers.pkg'
+
+  uninstall pkgutil: 'com.apple.pkg.SamsungPrinterDrivers'
+end
