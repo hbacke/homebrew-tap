6f112c35ac833d8e6084243000d90e6ed80d9a9c
Henrik Backe <henrik@backe.eu>
2016-04-30 23:19:46 +0200

    x

---
+++ Casks/avitools.rb
@@ -0,0 +1,11 @@
+cask 'avitools' do
+  version '3.4.3'
+  sha256 '6d36942498c539a303baa587cd1e63d28a5597394b3511ab79b2f62715690b56'
+
+  url "http://www.emmgunn.com/downloads/avitools#{version}.zip"
+  name 'AVItools'
+  homepage 'http://www.emmgunn.com/subtools/avitoolshome.html'
+  license :commercial
+
+  app "avitools#{version}/AVItools.app"
+end
