97f08987af672d32e32b96bd14991f19d652d17b
Henrik Backe <henrik.backe@evry.com>
2016-09-22 10:56:47 +0200

    x

--- Casks/juniper-pulse.rb
+++ Casks/juniper-pulse.rb
@@ -1,17 +1,13 @@
 cask 'juniper-pulse' do
-  version '5.1.5'
-  sha256 '00fcefd1a59354837f4ca8757d3a3e2963cced6ebb0d60821a0263e8a64771f1'
+  version '5.2.5'
+  sha256 '7c72dafbed610318cd63cfafa6d5aafe98b56174fbc394c7e4bd434786667933'
 
-  url 'https://www.dropbox.com/s/q32z6k0jdh1iyr6/ps-pulse-mac-5.1r5.1-b61437-installer.dmg?dl=1'
+  url 'https://www.dropbox.com/s/5o3yraf0ber5pmh/ps-pulse-mac-5.2r5-installer.dmg?dl=1'
   name 'Pulse Secure'
   homepage 'http://backe.eu'
   license :unknown
 
-  pkg 'JunosPulse.pkg'
+  pkg 'PulseSecure.pkg'
 
-  uninstall quit: [
-                   'net.juniper.PulseTray',
-                   'net.juniper.Junos-Pulse',
-                  ],
-            script: '/Library/Application Support/Juniper Networks/Junos Pulse/Uninstall.app/Contents/Resources/uninstall.sh'
+  uninstall script: '/Library/Application Support/Juniper Networks/Junos Pulse/Uninstall.app/Contents/Resources/uninstall.sh'
 end
