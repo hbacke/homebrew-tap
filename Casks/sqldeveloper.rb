898587c0b4a11dc4273ada3c8bd51f1dcb9b75be
Henrik Backe <Henrik.Backe@tietoevry.com>
2021-10-03 17:53:26 +0200

    Revert "x"
    This reverts commit 02b9055b19041c915ed3defd912ab4ee00889836.

---
+++ Casks/sqldeveloper.rb
@@ -0,0 +1,10 @@
+cask 'sqldeveloper' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.dropbox.com/s/1a5cf5as0yzl7ed/SQLDeveloper.tgz?dl=1'
+  name 'SQLDeveloper'
+  homepage 'http://backe.eu'
+
+  app 'SQLDeveloper.app'
+end
