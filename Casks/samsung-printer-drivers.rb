08ba24ee4ae00a0cf95a01466694722e950fe876
Henrik Backe <Henrik.Backe@tietoevry.com>
2021-10-05 22:53:32 +0200

    x

--- Casks/samsung-printer-drivers.rb
+++ Casks/samsung-printer-drivers.rb
@@ -2,7 +2,7 @@ cask 'samsung-printer-drivers' do
   version '2.6'
   sha256 'ecf283ff40df816e84d3a6148676114d2b4fe3f4074feb995c2c7e0be8be4964'
 
-  url 'http://support.apple.com/downloads/DL905/en_US/SamsungPrinterDrivers2.6.dmg'
+  url 'https://updates.cdn-apple.com/2019/cert/041-88836-20191011-73a473cb-fbb5-4e57-bc2f-14ccd60ea682/SamsungPrinterDrivers.dmg'
   name 'Samsung Printer Drivers'
   homepage 'http://backe.eu'
 
