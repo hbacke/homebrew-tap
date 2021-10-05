3718b880dc31b5bba15583d612bb5667f7204a48
Henrik Backe <Henrik.Backe@tietoevry.com>
2021-10-05 22:58:43 +0200

    x

--- Casks/samsung-printer-drivers.rb
+++ Casks/samsung-printer-drivers.rb
@@ -1,6 +1,6 @@
 cask 'samsung-printer-drivers' do
   version '2.6'
-  sha256 'ecf283ff40df816e84d3a6148676114d2b4fe3f4074feb995c2c7e0be8be4964'
+  sha256 'a5e88efea0bc6ce6a42d18cdb36f613541ebcc31f8122d9e2e036fb1249a8109'
 
   url 'https://updates.cdn-apple.com/2019/cert/041-88836-20191011-73a473cb-fbb5-4e57-bc2f-14ccd60ea682/SamsungPrinterDrivers.dmg'
   name 'Samsung Printer Drivers'
